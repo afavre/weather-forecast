e27d80e5eea316e0004c4dfc9126507cd852303c
Aurélien Favre <aurelien.favre2@gmail.com>
2019-07-28 14:06:57 +0100

    Docker-Compose orchestrator

--- src/main/java/com/afavre/weather/openweather/OpenWeatherConfig.java
+++ src/main/java/com/afavre/weather/openweather/OpenWeatherConfig.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "open-weather")
+@ConfigurationProperties(prefix = "openweather")
 public class OpenWeatherConfig {
 
     private String endpoint;
